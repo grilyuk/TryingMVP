b394034563793c986e263414fd035db55131fa8c
grilyuk <danilyuk1999@inbox.ru>
2023-01-13 00:27:26 +0300

    Initial Commit

---
+++ TryingMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TryingMVP
+//
+//  Created by Григорий Данилюк on 13.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
